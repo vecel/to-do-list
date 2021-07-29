c4e86ad324c0ecb5ce9ea367c06ec6bfa0c1f19a
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-07-29 17:36:03 +0200

    add new list div

--- src/page-loader.js
+++ src/page-loader.js
@@ -1,8 +1,18 @@
 export const DOMMainPageLoader = (() => {
-    
+    const pageContent = document.querySelector('main');
+
+    const _makeAddListButton = () => {
+        const buttonConatiner = document.createElement('div');
+        buttonConatiner.classList.add('add-new');
+        buttonConatiner.classList.add('center-content');
+        buttonConatiner.id = 'new-list';
+        buttonConatiner.textContent = 'Add new list';
+        return buttonConatiner;
+    }
 
     const load = () => {
-        console.log('loading...');
+        console.log('loading here');
+        pageContent.appendChild(_makeAddListButton());
     }
 
     return {
