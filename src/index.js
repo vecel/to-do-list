b0070ac4d976e70a21efebca300c702bb8e997cd
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-07-30 13:49:35 +0200

    Add new list event placeholder

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ import './style.css';
 import { DOMMainPageLoader } from './page-loader';
 import { ListFactory } from './factories';
 
+DOMMainPageLoader.load();
+
+const addNewList = () => {
+    console.log('new list');
+}
+
+const openList = (listTitle) => {
+    console.log(listTitle);
+}
+
+const listCards = document.querySelectorAll('div.list-card');
+const newListCardButton = document.querySelector('div#new-list');
+// console.log(newListCardButton);
+
+for (let listCard of listCards) {
+    listCard.addEventListener('click', openList);
+}
+newListCardButton.addEventListener('click', addNewList);
+
 let list1 = ListFactory();
 let list2 = ListFactory();
 
 // set lists under 'lists' key in local storage, if you change key name, change it in page-loader.js file, second argument is stringified array of list objects
 localStorage.setItem('lists', JSON.stringify([list1, list2]));
 
-DOMMainPageLoader.load();
 
 
