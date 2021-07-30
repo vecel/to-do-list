6960515b3fb93e33371d6b4dc9c9ea2c994cb913
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-07-30 15:22:01 +0200

    Add local storage management

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import { DOMMainPageLoader } from './page-loader';
 import { ListFactory, ListItemFactory } from './factories';
 import { STORAGE_LISTS_KEY } from './constants';
 
+initLocalStorageLists();
 DOMMainPageLoader.load();
-
-let listsArray = [];
+// let listsArray = [];
 
 const addNewList = () => {
     console.log('new list');
     let list = ListFactory();
+    addListToStorage(list);
+    DOMMainPageLoader.pushNewListCard(list.title);
 }
 
 const openList = (listTitle) => {
@@ -32,6 +34,16 @@ newListCardButton.addEventListener('click', addNewList);
 
 
 
-function updateListsInStorage() {
-    localStorage.setItem(STORAGE_LISTS_KEY, JSON.stringify(listsArray));
+function initLocalStorageLists() {
+    const listsArray = JSON.parse(localStorage.getItem(STORAGE_LISTS_KEY));
+    if (listsArray === null)
+        localStorage.setItem(STORAGE_LISTS_KEY, JSON.stringify([]));
+}
+
+function addListToStorage(newList) {
+    let lists = JSON.parse(localStorage.getItem(STORAGE_LISTS_KEY));
+    // if (lists === null) lists = [];
+    console.log(lists);
+    lists.push(newList);
+    localStorage.setItem(STORAGE_LISTS_KEY, JSON.stringify(lists));
 }
