b7e81b963f3bc2dcff080d6835b92667f41f625f
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-07-30 14:46:18 +0200

    Add constants.js

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const STORAGE_LISTS_KEY = 'myLists';
+
+export {
+    STORAGE_LISTS_KEY,
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import './style.css';
 import { DOMMainPageLoader } from './page-loader';
-import { ListFactory } from './factories';
+import { ListFactory, ListItemFactory } from './factories';
+import { STORAGE_LISTS_KEY } from './constants';
 
 DOMMainPageLoader.load();
 
+let listsArray = [];
+
 const addNewList = () => {
     console.log('new list');
+    let list = ListFactory();
 }
 
 const openList = (listTitle) => {
@@ -14,18 +18,20 @@ const openList = (listTitle) => {
 
 const listCards = document.querySelectorAll('div.list-card');
 const newListCardButton = document.querySelector('div#new-list');
-// console.log(newListCardButton);
 
 for (let listCard of listCards) {
     listCard.addEventListener('click', openList);
 }
 newListCardButton.addEventListener('click', addNewList);
 
-let list1 = ListFactory();
-let list2 = ListFactory();
+// let list1 = ListFactory();
+// let list2 = ListFactory();
 
 // set lists under 'lists' key in local storage, if you change key name, change it in page-loader.js file, second argument is stringified array of list objects
-localStorage.setItem('lists', JSON.stringify([list1, list2]));
+// localStorage.setItem(STORAGE_LISTS_KEY, JSON.stringify([]));
 
 
 
+function updateListsInStorage() {
+    localStorage.setItem(STORAGE_LISTS_KEY, JSON.stringify(listsArray));
+}
