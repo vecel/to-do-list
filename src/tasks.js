f400df680f42e01e2da3797a4ec0e2dcf3566bd6
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-08-01 17:32:34 +0200

    Sort tasks function

--- src/tasks.js
+++ src/tasks.js
@@ -8,27 +8,30 @@ const listId = getListIdFromUrl();
 const listStorageKey = LocalStorageManager.getTaskListKey(listId);
 let taskList = LocalStorageManager.getTaskListByListId(listId);
 
-// const listKey = taskList[listId].storageTaskListKey;
-// console.log(listKey);
+// sort array - function below
 
-console.log('list id: ' + getListIdFromUrl());
-console.log(LocalStorageManager.getTaskListByListId(listId));
+let customTasks = [{title: 'Lorem', done: false, dueDate: '02-08-2021'}, {title: 'Ipsum', done: true}];
+sortTaskList(customTasks);
 
-DOMTaskListLoader.load(listId);
+// console.log(customTasks);
 
+DOMTaskListLoader.renderTaskList(customTasks);
+
+const taskCards = document.querySelectorAll('div.task-container');
+const newTaskButton = document.querySelector('div#new-task');
 
 
 
 const addNewTask = () => {
     let task = ListItemFactory();
     taskList.push(task);
-    // console.log(taskList);
     DOMTaskListLoader.addTask();
     LocalStorageManager.addTask(listStorageKey, task);
 }
 
-const newTaskButton = document.querySelector('div#new-task');
-
+// for (let taskCard of taskCards) {
+//     const 
+// }
 newTaskButton.addEventListener('click', addNewTask);
 
 function getListIdFromUrl() {
@@ -36,4 +39,28 @@ function getListIdFromUrl() {
     const urlParameters = new URLSearchParams(queryString);
     const listId = urlParameters.get(URL_ID_PARAMETER_NAME);
     return listId;
+}
+
+// function addCustomTask() {
+//     let task = ListItemFactory();
+//     task.title = 'Lorem Ipsum';
+//     task.done = true;
+//     task.dueDate = 'tommorow';
+//     taskList.push(task);
+//     DOMTaskListLoader.addTask();
+//     LocalStorageManager.addTask(listStorageKey, task);
+// }
+
+function sortTaskList(taskList) {
+    taskList.sort((taskA, taskB) => {
+        if (taskA.done !== taskB.done) {
+            if (taskA.done) return 1;
+            return -1;
+        }
+
+        // dates
+
+        if (taskA.priority > taskB.priority) return -1;
+        return 0;
+    })
 }
